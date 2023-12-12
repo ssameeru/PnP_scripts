15128ea9862374cfcfdba3bdb2a752ede00d128f
Sameeruddin shaik <shaik.sameeruddin@intel.com>
2023-12-12 11:44:13 +0530

    power_bi: Create Helper script for renaming the power summary file
    As of Now, Power measurement data is generated with the Timestamps
    as directory name and File name is summary. This will rename the
    each KPI summary files as per the power BI database requirement
    
    Signed-off-by: Sameeruddin shaik <shaik.sameeruddin@intel.com>

---
+++ power_bi_db_helper_scripts/Power_Bi_helper.py
@@ -0,0 +1,14 @@
+import sys
+import Configreader as cr
+import Renamer as rn
+
+
+def main():
+    
+    if len(sys.argv) < 1:
+        print("Please provide the Work Week Root path of Power Measurements")
+
+    base_path = sys.argv[1]
+    
+if __name__ == "__main__":
+    main()
